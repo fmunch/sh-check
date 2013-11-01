80e859b39446d369aff6c9bea5bed799ae0e4070
Florent Munch <florent.munch@gmail.com>
2013-11-01 16:02:58 +0100

    Renamed variables

--- shcheck.sh
+++ shcheck.sh
@@ -2,71 +2,76 @@
 
 # sh-check 0.1
 # https://github.com/fmunch/sh-check
+#
+# Environment:
+#  - SHCHECK_NB_CHECKS: total number of checks
+#  - SHCHECK_LOG_END_HPA: columns at the left of the result tags
+#  - SHCHECK_FANCYTTY: if set to "true" / "false", forces colored / raw output
 
 
-: ${NB_CHECKS:=}
-CURRENT_CHECK=1
+: ${SHCHECK_NB_CHECKS:=}
+SHCHECK_CURRENT_CHECK=1
 
-: ${LOG_END_HPA:=70}
+: ${SHCHECK_LOG_END_HPA:=70}
 
 
 if which tput > /dev/null 2>&1; then
-  TPUT=$(which tput)
+  SHCHECK_TPUT=$(which tput)
 elif [ -x /usr/bin/tput ]; then
-  TPUT=/usr/bin/tput
+  SHCHECK_TPUT=/usr/bin/tput
 else
-  TPUT=
+  SHCHECK_TPUT=
 fi
 
-if [ -z "$FANCYTTY" -a -x "$TPUT" -a -n "$TERM" ] \
-    && "$TPUT" hpa "$LOG_END_HPA" > /dev/null 2>&1; then
-  FANCYTTY=true
+if [ -z "$SHCHECK_FANCYTTY" -a -x "$SHCHECK_TPUT" -a -n "$TERM" ] \
+    && "$SHCHECK_TPUT" hpa "$SHCHECK_LOG_END_HPA" > /dev/null 2>&1; then
+  SHCHECK_FANCYTTY=true
 fi
-case "$FANCYTTY" in
-  1|Y|true|yes) FANCYTTY=true;;
-  *) FANCYTTY=false;;
+case "$SHCHECK_FANCYTTY" in
+  1|Y|true|yes) SHCHECK_FANCYTTY=true;;
+  *) SHCHECK_FANCYTTY=false;;
 esac
 
-if [ "$FANCYTTY" = 'true' ]; then
-  COLOR_RED=$($TPUT setaf 1)
-  COLOR_GREEN=$($TPUT setaf 2)
-  COLOR_YELLOW=$($TPUT setaf 3)
-  COLOR_NORMAL=$($TPUT op)
+if [ "$SHCHECK_FANCYTTY" = 'true' ]; then
+  SHCHECK_COLOR_RED=$($SHCHECK_TPUT setaf 1)
+  SHCHECK_COLOR_GREEN=$($SHCHECK_TPUT setaf 2)
+  SHCHECK_COLOR_YELLOW=$($SHCHECK_TPUT setaf 3)
+  SHCHECK_COLOR_NORMAL=$($SHCHECK_TPUT op)
 else
-  COLOR_RED=
-  COLOR_GREEN=
-  COLOR_YELLOW=
-  COLOR_NORMAL=
+  SHCHECK_COLOR_RED=
+  SHCHECK_COLOR_GREEN=
+  SHCHECK_COLOR_YELLOW=
+  SHCHECK_COLOR_NORMAL=
 fi
 
 
 check() {
-  CHECK_COMMENT="$1"
-  CHECK_COMMAND="$2"
+  SHCHECK_CHECK_COMMENT="$1"
+  SHCHECK_CHECK_COMMAND="$2"
   shift 2
 
-  log_check_begin "$CHECK_COMMENT"
-  "$CHECK_COMMAND" "$@"
+  log_check_begin "$SHCHECK_CHECK_COMMENT"
+  "$SHCHECK_CHECK_COMMAND" "$@"
 
   log_check_end $?
-  CURRENT_CHECK=$(($CURRENT_CHECK + 1))
+  SHCHECK_CURRENT_CHECK=$(($SHCHECK_CURRENT_CHECK + 1))
 }
 
 log_check_begin() {
-  if [ -n "$NB_CHECKS" ]; then
-    printf "[%${#NB_CHECKS}s/%s] %s... " "$CURRENT_CHECK" "$NB_CHECKS" "$1"
+  if [ -n "$SHCHECK_NB_CHECKS" ]; then
+    printf "[%${#SHCHECK_NB_CHECKS}s/%s] %s... " "$SHCHECK_CURRENT_CHECK" "$SHCHECK_NB_CHECKS" "$1"
   else
-    printf "[%s] %s... " "$CURRENT_CHECK" "$1"
+    printf "[%s] %s... " "$SHCHECK_CURRENT_CHECK" "$1"
   fi
 }
 
 log_check_end() {
-  [ "$FANCYTTY" = 'true' ] && $TPUT hpa "$LOG_END_HPA"
+  [ "$SHCHECK_FANCYTTY" = 'true' ] && $SHCHECK_TPUT hpa "$SHCHECK_LOG_END_HPA"
   if [ "$1" = 0 ]; then
-    echo "[${COLOR_GREEN} OK ${COLOR_NORMAL}]"
+    echo "[${SHCHECK_COLOR_GREEN} OK ${SHCHECK_COLOR_NORMAL}]"
   elif [ "$1" = 255 ]; then
-    echo "[${COLOR_YELLOW}WARN${COLOR_NORMAL}]"
+    echo "[${SHCHECK_COLOR_YELLOW}WARN${SHCHECK_COLOR_NORMAL}]"
   else
-    echo "[${COLOR_RED}FAIL${COLOR_NORMAL}]"
+    echo "[${SHCHECK_COLOR_RED}FAIL${SHCHECK_COLOR_NORMAL}]"
   fi
 }
